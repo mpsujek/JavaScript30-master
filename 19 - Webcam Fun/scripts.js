8350e6844b01a1261cec30571b5c16546dee9323
Mateusz Psujek <mateusz.psujek@gmail.com>
2017-08-15 23:10:08 +0200

    half way

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,42 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch(err => {
+      console.error('oh no', err);;
+    })
+}
+
+function paintToCanvas(){
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return  setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height
+    )
+  }, 16);
+}
+
+function takePhoto(){
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download','picture');
+  link.innerHTML=`<img src="${data}" alt="photo">`;
+  strip.insertBefore(link, strip.firsChild);
+}
+
+getVideo();
+video.addEventListener('canplay', paintToCanvas)
