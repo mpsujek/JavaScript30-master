84ef582904b9bdd5c11d09d9e43f142617801cb1
Mateusz Psujek <mateusz.psujek@gmail.com>
2017-08-16 08:21:32 +0200

    webcam fun

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -23,8 +23,13 @@ function paintToCanvas(){
   canvas.height = height;
 
   return  setInterval(() => {
-    ctx.drawImage(video, 0, 0, width, height
-    )
+    ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0,0, width, height);
+    // pixels = redEffect(pixels);
+    // pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.1;
+    pixels = greenScreen(pixels);
+    ctx.putImageData(pixels,0,0);
   }, 16);
 }
 
@@ -38,6 +43,51 @@ function takePhoto(){
   link.setAttribute('download','picture');
   link.innerHTML=`<img src="${data}" alt="photo">`;
   strip.insertBefore(link, strip.firsChild);
+}
+
+function redEffect(pixels){
+  for (let i = 0; i < pixels.data.length ; i += 4 ){
+  pixels.data[i + 0] = pixels.data[i + 0] + 200;
+  pixels.data[i + 1] = pixels.data[i + 1] -50;
+  pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels){
+  for (let i = 0; i < pixels.data.length ; i += 4 ){
+  pixels.data[i - 150] = pixels.data[i + 0] ;
+  pixels.data[i + 500] = pixels.data[i + 1] ;
+  pixels.data[i - 150] = pixels.data[i + 2] ;
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
 }
 
 getVideo();
